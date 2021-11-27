dd8c8193fba60da10d51c6c6af2a4ebf9acf3983
souvikghosh <Sayali1@>
2021-11-27 10:50:18 -0800

    basic if else costruct and exmaples

---
+++ control-flow/if-else-elif.py
@@ -0,0 +1,24 @@
+# We often only want certain code to execute when a condition has been met
+# if
+# elif
+# else
+
+#if True:
+    #print('Its True!')
+
+# hungry = False
+# if hungry:
+#     print('FEED ME!')
+# else:
+#     print('I am not hungry!')
+
+loc = 'Bank'
+if loc == 'Auto Shop':
+    print('Cars are cool')
+elif loc == 'Bank':
+    print('Money is cool')
+elif loc == 'Store':
+    print('Welcome ot the store')
+else:
+    print('I do not know much')
+
