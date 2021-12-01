da2616911690803d1815b008c54e6a3c38a6045c
souvikghosh <Sayali1@>
2021-12-01 10:07:05 -0800

    built in methods and where to find help for the same

---
+++ methods-functions/python-methods.py
@@ -0,0 +1,10 @@
+# built in object methods
+# mylist = [1, 2, 3]
+# mylist.append(4)
+# print(mylist)
+# print(mylist.pop())
+# print(mylist)
+
+# help(mylist.insert)
+
+# refer to official documentation for more details on all the methods
