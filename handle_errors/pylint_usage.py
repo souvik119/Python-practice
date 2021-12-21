36e5d083074cd1480e94ee0fc50f19dd1ab1a2dd
souvikghosh <Sayali1@>
2021-12-21 09:31:07 -0800

    basic usgae of pylint for checking code quality

---
+++ handle_errors/pylint_usage.py
@@ -0,0 +1,23 @@
+# pylint is a library that looks at your code and reports back possible issues - styling
+# usage is pylint <script name.py> -r y
+
+
+# a = 1
+# b = 2
+# print(a)
+# print(B) #intentional mistake
+
+'''
+A very simple script
+'''
+
+def myfunc():
+    '''
+    A simple function
+    '''
+    first = 1
+    second = 2
+    print(first)
+    print(second)
+
+myfunc()
